1855fa045271c8c6ea1a69c66f3659072cdc692d
Guilebaldo <guilebaldodev@gmail.com>
2024-09-08 10:30:00 -0600

    admin sidebar

--- src/components/AdminSideBar.jsx
+++ src/components/AdminSideBar.jsx
@@ -1,66 +1,174 @@
 import { Link } from 'react-router-dom'
-
 import {Outlet} from 'react-router-dom'
+import '../css/admin-sidebar.css'
+import { useState } from 'react';
+
 
 const SideBar = () => {
+
+    const [activeMenu, setActiveMenu] = useState(null);
+
+
+    const toggleMenu=(menu)=>{
+        if(activeMenu===menu){
+            setActiveMenu(null)
+        }else{
+            setActiveMenu(menu)
+        }
+
+    }
+
+
     return ( 
     <>
     <div className="super-container">
 
-    <div className="side-bar-container">
-        <div className="side-bar-head">
-            <div className="side-bar-title">
-                <img src="/white-logo.png"></img>
-                <h3>Club EXANI</h3>
+    <div className="sidebar-container">
+        <div className="sidebar-top">
+        <div className="sidebar-header">
+            <div className="sidebar-title">
+                <img src="/icons/logo2.png"></img>
+                <h3>Club<span className='primary-color'>Exani</span></h3>
             </div>
-            <div className="side-bar-options">
+        </div>
+            <div className="sidebar-options">
+                <Link to="/panel-control/preguntas">
+                    <img src="/icons/admin-sidebar/home-icon.png" alt="" />
+                    <p>Dashboard</p>
+                </Link>
+
+
+                <div className="option-menu">
+                    <div className="option-menu-title" onClick={()=>{
+                        toggleMenu('preguntas')
+                    }}>
+                        <div className="title-right">
+                        <img src="/icons/admin-sidebar/question-icon.png" alt="" />
+                        <p>Preguntas</p>
+                        </div>
+                        <img src="/icons/admin-sidebar/forward-icon.png" alt="" />
+                    </div>
+
+                    <div className={`content ${activeMenu==="preguntas"?"show":""}`}>
+
+                    <Link to="/panel-control/preguntas">
+                        <img src="/icons/admin-sidebar/round-icon.png" alt="" />
+                        <p>Lista</p>
+                    </Link>
+
+                    <Link to="/panel-control/preguntas">
+                        <img src="/icons/admin-sidebar/round-icon.png" alt="" />
+                        <p>Añadir pregunta</p>
+                    </Link>
+                    </div>
+
+                </div>
+                <div className="option-menu">
+                    <div className="option-menu-title"  onClick={()=>{
+                        toggleMenu('lecciones')
+                    }}>
+                        <div className="title-right">
+                        <img src="/icons/admin-sidebar/open-book.png" alt="" />
+                        <p>Lecciones</p>
+                        </div>
+                        <img src="/icons/admin-sidebar/forward-icon.png" alt="" />
+                    </div>
+
+                    <div className={`content ${activeMenu==="lecciones"?"show":""}`}>
+
+                    <Link to="/panel-control/preguntas">
+                        <img src="/icons/admin-sidebar/round-icon.png" alt="" />
+                        <p>Lista</p>
+                    </Link>
+
+                    <Link to="/panel-control/preguntas">
+                        <img src="/icons/admin-sidebar/round-icon.png" alt="" />
+                        <p>Añadir leccion</p>
+                    </Link>
+                    </div>
+
+                </div>
+      
+                <div className="option-menu">
+                    <div className="option-menu-title" onClick={()=>{
+                        toggleMenu('simuladores')
+                    }}>
+                        <div className="title-right">
+                        <img src="/icons/admin-sidebar/writing-icon.svg" alt="" />
+                        <p>Simuladores</p>
+                        </div>
+                        <img src="/icons/admin-sidebar/forward-icon.png" alt="" />
+                    </div>
+
+                    <div className={`content ${activeMenu==="simuladores"?"show":""}`}>
+
+                    <Link to="/panel-control/preguntas">
+                        <img src="/icons/admin-sidebar/round-icon.png" alt="" />
+                        <p>Lista</p>
+                    </Link>
+
+                    <Link to="/panel-control/preguntas">
+                        <img src="/icons/admin-sidebar/round-icon.png" alt="" />
+                        <p>Construir simulador</p>
+                    </Link>
+                    </div>
+
+                </div>
+
+                <div className="option-menu">
+                    <div className="option-menu-title" onClick={()=>{
+                        toggleMenu('cuestionarios')
+                    }}>
+                        <div className="title-right">
+                        <img src="/icons/admin-sidebar/test.png" alt="" />
+                        <p>Cuestionarios</p>
+                        </div>
+                        <img src="/icons/admin-sidebar/forward-icon.png" alt="" />
+                    </div>
+
+                    <div className={`content ${activeMenu==="cuestionarios"?"show":""}`}>
+
+                    <Link to="/panel-control/preguntas">
+                        <img src="/icons/admin-sidebar/round-icon.png" alt="" />
+                        <p>Lista</p>
+                    </Link>
+
+                    <Link to="/panel-control/preguntas">
+                        <img src="/icons/admin-sidebar/round-icon.png" alt="" />
+                        <p>Construir cuestionario</p>
+                    </Link>
+                    </div>
+
+                </div>
+
+                <Link to="/panel-control/preguntas">
+                    <img src="/icons/admin-sidebar/user-icon.png" alt="" />
+                    <p>Usuarios</p>
+                </Link>
+
+
+
+        
+            </div>
+
+        </div>
+
+
+            <div className="sidebar-footer">
+
             <Link to="/panel-control/preguntas">
-                <img src="/icons/question-icon.png" alt="" />
-                <h4>Preguntas</h4>
-            </Link>
+                    <img src="/icons/admin-sidebar/dark-mode-icon.png" alt="" />
+                    <p>Tema</p>
+                </Link>
 
-            <Link to="/">
-                <img src="/icons/exam-icon.png" alt="" />
-                <h4>Examenes</h4>
-            </Link>
-            <Link to="/">
-                <img src="/icons/subject-icon.png" alt="" />
-                <h4>Lecciones</h4>
-            </Link>            
-            <Link to="/">
-                <img src="/icons/questionary-icon.png" alt="" />
-                <h4>Cuestionarios</h4>
-            </Link>            
-            <Link to="/">
-                <img src="/icons/analytics-icon.png" alt="" />
-                <h4>Analiticas</h4>
-            </Link>
-            <Link to="/">
-                <img src="/icons/discount-icon.png" alt="" />
-                <h4>Descuentos</h4>
-            </Link>
+
+
+                <Link to="/panel-control/preguntas">
+                    <img src="/icons/admin-sidebar/log-out-icon.png" alt="" />
+                    <p>Cerrar sesion</p>
+                </Link>
+
             </div>
-        </div>
-        <div className="side-bar-footer">
-        <div className="side-bar-options">
-            <Link to="/">
-                <img src="/icons/dark-mode-icon.png" alt="" />
-                <h4>Tema</h4>
-            </Link>
-
-            <Link to="/">
-                <img src="/icons/return-icon.png" alt="" />
-                <h4>Salir</h4>
-            </Link>
-         
-            <Link to="/">
-                <img src="/icons/log-out-icon.png" alt="" />
-                <h4>Cerrar sesion</h4>
-            </Link>
-         
-            </div>
-        </div>
-
     </div>
 <Outlet></Outlet>    
     </div>
